5649d54d2005a1336af39222dc2b1bdd5210d33e
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-22 17:21:29 -0700

    Board#render

--- minesweeper/board.rb
+++ minesweeper/board.rb
@@ -8,17 +8,17 @@ class Board
   }
   def self.place_mines(difficulty)
     dimention, num_mines = DIFFICULTIES[difficulty]
-    size = dimention.inject(:*)
+    board_size = dimention.inject(:*)
     grid = Array.new(dimention[0]) { Array.new(dimention[1]) }
 
     tiles = []
-    (size - num_mines).times { tiles << Tile.new(false) }
+    (board_size - num_mines).times { tiles << Tile.new(false) }
     num_mines.times { tiles << Tile.new(true) }
     tiles.shuffle!
 
-    grid.each do |row|
-      row.each do |tile|
-        tile = tiles.pop
+    grid.each_with_index do |row, row_i|
+      row.each_with_index do |tile, col_i|
+        grid[row_i][col_i] = tiles.pop
       end
     end
 
@@ -28,4 +28,34 @@ class Board
   def initialize(grid)
     @grid = grid
   end
+
+  def width
+    @grid[0].length
+  end
+
+  def height
+    @grid.length
+  end
+
+  attr_reader :grid
+  alias_method :rows, :grid
+
+  def render
+    puts "  " + (0...width).to_a.join(" ")
+    rows.each_with_index do |row, row_i|
+      print row_i.to_s + "|"
+      
+      row.each do |tile|
+        print "#{tile_render(tile)}|"
+      end
+      puts 
+    end
+  end
+
+  def tile_render(tile)
+    return "F" if tile.flagged && tile.revealed
+    return "_" if tile.revealed && !tile.is_bomb
+    return " "
+  end
+  
 end
