769a345243a305d3465cc4b74ee1f5ac1843ddbf
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-22 19:28:11 -0700

    basic game logic

--- minesweeper/minesweeper.rb
+++ minesweeper/minesweeper.rb
@@ -2,21 +2,28 @@ require_relative "board"
 
 class Minesweeper
   def initialize(difficulty)
-    @board = Board.place_mine(difficulty)
+    @board = Board.place_mines(difficulty)
   end
 
   def play_turn
+    system("clear")
     @board.render
     pos = get_pos
     action = get_action
 
-    record_action(pos, action)
+    if invalid_action?(pos, action)
+      inform_invalid_action(pos, action) 
+    else
+      record_action(pos, action)
+    end
   end
 
   def run
     until game_over?
       play_turn
     end
+    sybtem("clear")
+    @board.render
   end
 
   def game_over?
@@ -28,15 +35,61 @@ class Minesweeper
   end
 
   def get_pos
+    pos = nil
+
+    until pos && valid_pos?(pos)
+      puts "please enter a position. Ex. '3,4' > "
+
+      begin
+        pos = parse_pos(gets.chomp)
+      rescue
+        puts "Did you type a comma to separate the numbers? Try again"
+        pos = nil
+      end
+
+      puts "Invalid position. try gagin!" if !valid_pos?(pos)
+    end
+
+    pos
+  end
+
+  def parse_pos(str)
+    str.split(",").map { |i| Integer(i) }
   end
 
   def get_action
+    action = nil
+
+    until [1, 2].include?(action)
+      puts "please enter 1 or 2" if !action.nil?
+      puts "Enter '1' to place or remove a flag"
+      puts "Enter '2' to reveal a tile"
+      print "What do you want to do > "
+      action = gets.chomp[0].to_i
+    end
+
+    action
   end
 
   def record_action(pos, action)
+    action == 1 ? @board[pos].toggle_flag : @board[pos].reveal
   end
 
   def valid_pos?(pos)
+    pos.length == 2 &&
+      pos[0].between?(0, @board.height) &&
+      pos[1].between?(0, @board.width)
+  end
+
+  def invalid_action?(pos, action)
+    @board[pos].revealed || 
+      (action == 2 && @board[pos].flagged)
+  end
+
+  def inform_invalid_action(pos, action)
+    puts "You can't flag a revealed tile." if action == 1 && @board[pos].revealed
+    puts "It's been revealed." if action == 2 && @board[pos].revealed
+    puts "You can't reveal a flag!" if action == 2 && @board[pos].flagged
   end
 
 end
