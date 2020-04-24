94e63c31b06db7bba1e1669b079e0e34c6ed78cd
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-24 12:51:23 -0700

    finish polytreenode project

---
+++ data_structures/intro_polytreenode/lib/00_tree_node.rb
@@ -0,0 +1,49 @@
+require_relative "searchable"
+
+class PolyTreeNode
+
+  include Searchable
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  attr_reader :value, :children, :parent
+  
+  def parent=(node)
+    remove_self_from_children_list if parent_exists
+    @parent = node
+    add_self_to_children_list if parent_exists
+  end
+
+  def add_child(node)
+    node.parent = self
+    @children << node unless is_child?(node)
+  end
+
+  def remove_child(node)
+    raise "This is not a child node" unless is_child?(node)
+    node.parent = nil
+    @children.delete(node)
+  end
+
+  def is_child?(node)
+    @children.include?(node)
+  end
+
+  private
+
+  def parent_exists
+    !@parent.nil?
+  end
+
+  def remove_self_from_children_list
+    @parent.children.delete(self)
+  end
+
+  def add_self_to_children_list
+    @parent.children << self unless @parent.is_child?(self)
+  end
+end
