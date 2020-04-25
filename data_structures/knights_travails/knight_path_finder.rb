5609eb965903a2ebdd96598f21d0d57f09f50b40
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-25 16:53:31 -0700

    finish find_path

--- data_structures/knights_travails/knight_path_finder.rb
+++ data_structures/knights_travails/knight_path_finder.rb
@@ -1,5 +1,4 @@
 require_relative "poly_tree_node"
-
 class KnightPathFinder
 
   def self.valid_moves(pos)
@@ -7,15 +6,17 @@ class KnightPathFinder
     moves += moves.map(&:reverse)
     moves.map do |move|
       move.map.with_index { |el, idx| el + pos[idx] }
-    end.reject { |move| move.any? { |el| el < 0 } }
+    end.select { |move| KnightPathFinder.valid_pos?(move) }
   end
 
-  attr_reader :considered_positions, :root_node
+  attr_reader :start_pos
 
   def initialize(start_pos)
     @start_pos = start_pos
     @root_node = PolyTreeNode.new(start_pos)
     @considered_positions = []
+
+    build_move_tree
   end
 
   def new_move_positions(pos)
@@ -31,7 +32,6 @@ class KnightPathFinder
     until queue.empty?
       curr_node = queue.shift
       curr_pos = curr_node.value
-      # break if curr_pos == pos
       new_moves = new_move_positions(curr_pos)
       children_nodes = new_moves.map { |new_pos| PolyTreeNode.new(new_pos) }
       children_nodes.each do |node|
@@ -42,10 +42,22 @@ class KnightPathFinder
   end
 
   def find_path(end_pos)
-
+    node = @root_node.bfs(end_pos)
+    trace_path_back(node)
   end
 
   def trace_path_back(node)
+    path = []
+    curr_node = node
+    until curr_node.nil?
+      path.unshift(curr_node.value)
+      curr_node = curr_node.parent
+    end
+
+    path
   end
 
+  def self.valid_pos?(pos)
+    pos.all? { |dimension| dimension.between?(0, 7) }
+  end
 end
