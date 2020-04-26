68cbb8047261ee4d7e7f3cf08907939a44643da7
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-25 18:23:57 -0700

    finish ttt ai phase 2

--- data_structures/tic_tac_toe_ai/lib/super_computer_player.rb
+++ data_structures/tic_tac_toe_ai/lib/super_computer_player.rb
@@ -2,6 +2,14 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    ttt_node = TicTacToeNode.new(game.board, mark)
+    tie_positions = []
+    ttt_node.children.each do |child|
+      return  child.prev_move_pos if child.winning_node?(mark)
+      tie_positions << child.prev_move_pos if !child.losing_node?(mark)
+    end 
+    raise if tie_positions.empty?
+    tie_positions.sample
   end
 end
 
