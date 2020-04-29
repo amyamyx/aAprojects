3af70efd014466e5b72347b59d6904577dfaea9e
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-28 20:14:54 -0700

    modify slideable module

--- ruby/chess/pieces/modules/slideable.rb
+++ ruby/chess/pieces/modules/slideable.rb
@@ -14,29 +14,27 @@ module Slideable
   ]
 
   def horizontal_dirs
-    move_dirs(HORIZONTAL_DIRS)
   end
 
   def diagonal_dirs
-    move_dirs(DIAGONAL_DIRS)
   end
 
   def moves
-  end
-
-  private
-
-  def move_dirs(directions)
     positions = []
-    directions.each do |move|
+    move_dirs.each do |move|
       new_pos = @pos.map_with_index { |i, idx| i + move[idx] }
       while @board.valid_pos?(new_pos)
         positions << new_pos
         new_pos = new_pos.map_with_index { |i, idx| i + move[idx]}
       end
     end
-    
+
     positions
+  end
+
+  private
+
+  def move_dirs
   end
 
   def grow_unblocked_moves_in_dir(dx, dy)
