236a48dd58566719ad00a78de5b0c6a42e0f4047
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-28 20:55:39 -0700

    create and setup pawn piece

---
+++ ruby/chess/pieces/pawn.rb
@@ -0,0 +1,28 @@
+require_relative "piece"
+
+class Pawn < Piece
+  def initialize(color, board, pos)
+    super(color, board, pos)
+  end
+
+  def symbol
+    "♟"
+  end
+
+  def move_dirs
+  end
+
+  private
+  
+  def at_start_row?
+  end
+
+  def forward_dir
+  end
+
+  def forwawrd_steps
+  end
+
+  def side_attacks
+  end
+end
