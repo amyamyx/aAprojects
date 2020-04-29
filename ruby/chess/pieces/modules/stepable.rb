2007b1b07d78934d372d997fdc2a06900d3c4d14
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-28 20:38:22 -0700

    modify stepable module

--- ruby/chess/pieces/modules/stepable.rb
+++ ruby/chess/pieces/modules/stepable.rb
@@ -1,5 +1,11 @@
 module Stepable
   def moves
+    positions = []
+    move_diffs.each do |move|
+      new_pos = @pos.map_with_index { |i, idx| i + move[i] }
+      positions << new_pos if @board.valid_pos?(new_pos)
+    end
+    positions
   end
 
   private
