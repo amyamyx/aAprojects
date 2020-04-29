f2cb9365c5496b3f91231b11f6d2fa13d04ba99d
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-04-29 15:36:39 -0700

    minor refactor

--- ruby/chess/board.rb
+++ ruby/chess/board.rb
@@ -8,32 +8,7 @@ require_relative "pieces/knight"
 require_relative "pieces/null_piece"
 
 class Board
-  def place_pieces
-    @rows.each_with_index do |row, row_i|
-      color = row_i < 2 ? :red : :blue
-      if row_i == 1 || row_i == 6
-        row.map!.with_index { |x, col_i| Pawn.new(color, self, [row_i, col_i]) }
-      elsif row_i == 0 || row_i == 7
-        row.map!.with_index do |x, col_i|
-          args = [color, self, [row_i, col_i]]
-          if col_i == 0 || col_i == 7
-            Rook.new(*args)
-          elsif col_i == 1 || col_i == 6
-            Knight.new(*args)
-          elsif col_i == 2 || col_i == 5
-            Bishop.new(*args)
-          elsif col_i == 3
-            Queen.new(*args)
-          else
-            King.new(*args)
-          end
-        end
-      else
-        row.map! { |i| NullPiece.instance }
-      end
-    end
-  end
-  
+
   def initialize
     @rows = Array.new(8) { Array.new(8) }
 
@@ -52,7 +27,7 @@ class Board
     @rows[row][col] = val
   end
 
-  def move_piece(start_pos, end_pos) 
+  def move_piece!(start_pos, end_pos) 
     moving_piece = self[start_pos]
 
     raise "There's no piece here" if moving_piece.nil?
@@ -76,5 +51,33 @@ class Board
       puts " -------------------------"
     end
     nil
+  end
+
+  private
+
+    def place_pieces
+    @rows.each_with_index do |row, row_i|
+      color = row_i < 2 ? :red : :blue
+      if row_i == 1 || row_i == 6
+        row.map!.with_index { |x, col_i| Pawn.new(color, self, [row_i, col_i]) }
+      elsif row_i == 0 || row_i == 7
+        row.map!.with_index do |x, col_i|
+          args = [color, self, [row_i, col_i]]
+          if col_i == 0 || col_i == 7
+            Rook.new(*args)
+          elsif col_i == 1 || col_i == 6
+            Knight.new(*args)
+          elsif col_i == 2 || col_i == 5
+            Bishop.new(*args)
+          elsif col_i == 3
+            Queen.new(*args)
+          else
+            King.new(*args)
+          end
+        end
+      else
+        row.map! { |i| NullPiece.instance }
+      end
+    end
   end
 end
