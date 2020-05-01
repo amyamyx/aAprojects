bc7c4796ca7306bd72c50080f5e84e0aefc47090
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-05-01 15:02:50 -0700

    pawn moves logic

--- ruby/chess/pieces/pawn.rb
+++ ruby/chess/pieces/pawn.rb
@@ -18,11 +18,27 @@ class Pawn < Piece
   end
 
   def forward_dir
+    d_row = @color == :red ? 1 : -1
+    row, col = @pos
+    [[row + d_row, col], [row + d_row * 2, col]].select { |i| @board.valid_pos?(i) }
   end
 
   def forwawrd_steps
   end
 
   def side_attacks
+    attack_pos = []
+    row, col = @pos
+    d_row = @color == :red ? 1 : -1
+    
+    [1, -1].each do |d_col|
+      new_pos = [row + d_row, col + d_col]
+      attack_piece = attack_piece
+      if !attack_piece.nil? !&& attack_piece.color.nil?
+        attack_pos << new_pos if attack_piece.color != @color
+      end
+    end
+
+    attack_pos
   end
 end
