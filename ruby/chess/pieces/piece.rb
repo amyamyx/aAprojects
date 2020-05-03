10f2897b7ade10542aaea1581e57fce297e3c819
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-05-03 02:52:26 -0700

    modify attr_accessor pos

--- ruby/chess/pieces/piece.rb
+++ ruby/chess/pieces/piece.rb
@@ -1,7 +1,8 @@
 require "colorize"
 
 class Piece
-  attr_reader :color, :pos
+  attr_reader :color
+  attr_accessor :pos
   def initialize(color, board, pos)
     @color = color
     @board = board
