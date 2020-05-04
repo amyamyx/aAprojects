d0121d9f96069ed20860b99c6f55b6fa68abb9d3
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-05-03 17:57:35 -0700

    HumanPlayer#make_move forces moves upon player's selection

--- ruby/chess/players/human_player.rb
+++ ruby/chess/players/human_player.rb
@@ -1,10 +1,33 @@
 require_relative "player"
 
 class HumanPlayer < Player
-  def initialize
+  def initialize(color, display)
     super(color, display)
+    @cursor = display.cursor
   end
 
   def make_move(_board)
+    cursor = @display.cursor
+
+    start_pos = get_pos
+    selected_piece = _board[start_pos]
+    selected_piece.toggle_selected
+    end_pos = get_pos
+    
+    _board.move_piece!(start_pos, end_pos)
+    selected_piece.toggle_selected
+
+  end
+
+  def get_pos
+    system("clear")
+    @display.render
+    pos = @cursor.get_input
+    while pos.nil?
+      system("clear")
+      @display.render
+      pos = @cursor.get_input 
+    end
+    pos
   end
 end
