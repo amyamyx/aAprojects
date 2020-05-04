b646496969ca76a616deeb063da885c819315eb6
Tiffany Tang <amyamyxtiffany@gmail.com>
2020-05-03 17:58:38 -0700

    plan out basic play turn logic

--- ruby/chess/game.rb
+++ ruby/chess/game.rb
@@ -7,11 +7,19 @@ class Game
   def initialize
     @board = Board.new
     @display = Display.new(@board)
-    @players = {}
-    @current_player = nil
+    @players = { 
+      blue: HumanPlayer.new(:blue, @display),
+      red: HumanPlayer.new(:red, @display)
+    }
+    @current_player = @players[:blue]
   end
 
   def play
+    until over?
+      notify_players
+      @current_player.make_move(@board)
+      swap_turn!
+    end
   end
 
   private
@@ -20,5 +28,18 @@ class Game
   end
 
   def swap_turn!
+    @current_player = @current_player == @players[:blue] ? @players[:red] : @players[:blue]
   end
+
+  def over?
+    # TODO: @board.tie? and @board.win?
+
+    # @board.tie? || @board.win?
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  g = Game.new
+  g.play
 end
