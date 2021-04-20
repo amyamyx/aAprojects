63397f08767b5f01b633e51f865bea559353eeee
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-04-20 16:32:14 +0800

    finished hash set practice

--- ruby/lru_cache/lib/p03_hash_set.rb
+++ ruby/lru_cache/lib/p03_hash_set.rb
@@ -7,18 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    if !include?(key)
+      resize! if @count == num_buckets
+      @count += 1
+      self[key] << key
+    end    
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @count -=1
+      self[key].delete(key)
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +37,9 @@ class HashSet
   end
 
   def resize!
+    elements = @store.flatten
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    elements.each { |el| insert(el) }
   end
 end
