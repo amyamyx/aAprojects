0c069d6cab3f0d6c717b21c3dddeb54b63d0aa2c
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-04-20 17:46:57 +0800

    finished linked list practice

--- ruby/lru_cache/lib/p04_linked_list.rb
+++ ruby/lru_cache/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -16,11 +17,22 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
+
+    @prev, @next = nil, nil
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +41,63 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    each { |node| return node.val if node.key == key }
   end
 
   def include?(key)
+    each { |node| return true if node.key == key }
+    false
   end
 
   def append(key, val)
+    last_node = last
+    new_node = Node.new(key, val)
+
+    # breaking the links between last node and tail
+    last_node.next = new_node
+    @tail.prev = new_node
+    
+    # adding links between the new node and the last node and tail
+    new_node.prev = last_node
+    new_node.next = @tail
   end
 
   def update(key, val)
+    each { |node| node.val = val if node.key == key }
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.remove
+        break
+      end
+    end
   end
 
   def each
+    current_node = @head.next
+
+    while current_node != @tail
+      yield(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
