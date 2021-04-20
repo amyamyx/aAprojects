55d1556a13fdc6d58307dea7038b8efb495ce3c4
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-04-20 16:26:35 +0800

    finished practicing hashing

--- ruby/lru_cache/lib/p02_hashing.rb
+++ ruby/lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,26 @@ end
 
 class Array
   def hash
+    num = 0
+    self.each_with_index do |el, idx|
+      num += (el.to_s.length + idx * 30).hash
+    end
+
+    num.hash
   end
 end
 
 class String
   def hash
+    letters = ("a".."z").to_a + ("A".."Z").to_a
+   
+    num = 0
+
+    self.each_char.with_index do |ch, idx|
+      num += (letters.index(ch) + idx.hash).hash
+    end
+
+    num.hash
   end
 end
 
@@ -16,6 +31,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    num = 0
+
+    self.each do |k, v|
+      num += (k.hash + v.hash).hash
+    end
+
+    num.hash
   end
 end
