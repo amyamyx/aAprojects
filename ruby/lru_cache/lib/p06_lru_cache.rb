5f3f422e28fbb590354a2aacef444c2c7ddb2641
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-04-20 20:59:27 +0800

    finished lru cache practice

--- ruby/lru_cache/lib/p06_lru_cache.rb
+++ ruby/lru_cache/lib/p06_lru_cache.rb
@@ -14,6 +14,18 @@ class LRUCache
   end
 
   def get(key)
+    node = @map[key]
+
+    if node
+      val = node.val
+      update_node!(node)
+    else
+      eject! if count == @max
+      val = calc!(key)
+      @map[key] = @store.append(key, val)
+    end
+
+    return val
   end
 
   def to_s
@@ -24,12 +36,19 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    @prc.call(key)
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    key, val = node.key, node.val
+    node.remove
+    @map[key] = @store.append(key, val)
   end
 
   def eject!
+    key = @store.first.key
+    @store.remove(key)
+    @map.delete(key)
   end
 end
