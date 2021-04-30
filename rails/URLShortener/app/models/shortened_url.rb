bc367189506f0f15e54129552c1c17eb005eddd3
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-04-30 21:58:14 +0800

    refactored

--- rails/URLShortener/app/models/shortened_url.rb
+++ rails/URLShortener/app/models/shortened_url.rb
@@ -1,15 +1,26 @@
 class ShortenedUrl < ApplicationRecord
   validates :short_url, :long_url, presence: true, uniqueness: true
   
-  def self.random_code
-    url = nil
+  belongs_to :submitter,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: "User"
 
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: "Visit"
+  
+  has_many :visitors,
+    Proc.new { distinct },
+    through: :visits,
+    source: :visitor
+
+  def self.random_code
     loop do
       url = SecureRandom.urlsafe_base64(16)
-      break unless self.exists?(short_url: url)
+      return url unless self.exists?(short_url: url)
     end
-
-    url
   end
 
   def self.generate_short_url(user, long_url)
@@ -25,24 +36,14 @@ class ShortenedUrl < ApplicationRecord
   end
 
   def num_uniques
-    Visit.where({ shortened_url_id: @id }).select(:user_id).distinct.count
-    # visitors.distinct.count
+    visitors.count
   end
 
   def num_recent_uniques
-  end
-
-  belongs_to :submitter,
-    primary_key: :id,
-    foreign_key: :user_id,
-    class_name: "User"
-
-  has_many :visits,
-    primary_key: :id,
-    foreign_key: :shortened_url_id,
-    class_name: "Visit"
-  
-  has_many :visitors,
-    through: :visits,
-    source: :visitor
+    visits
+      .select(:user_id)
+      .where("created_at > ?", 10.minutes.ago)
+      .distinct
+      .count
+  end  
 end
