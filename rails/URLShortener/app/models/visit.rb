d38c9e4ddebb42cdb010b2bc87c999499f0456e1
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-02 00:48:58 +0800

    annotate schema into models

--- rails/URLShortener/app/models/visit.rb
+++ rails/URLShortener/app/models/visit.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: visits
+#
+#  id               :bigint           not null, primary key
+#  user_id          :integer          not null
+#  shortened_url_id :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class Visit < ApplicationRecord
 
   def self.record_visit!(user, shortened_url)
