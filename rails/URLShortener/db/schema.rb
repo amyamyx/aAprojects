902c726038fe5c0fb0e94774ee110800c6192568
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-02 01:49:34 +0800

    added new column premium to users table

--- rails/URLShortener/db/schema.rb
+++ rails/URLShortener/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_164942) do
+ActiveRecord::Schema.define(version: 2021_05_01_172636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_04_30_164942) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "premium", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
