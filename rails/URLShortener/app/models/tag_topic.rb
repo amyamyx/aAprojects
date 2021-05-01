a0f77ad6e449f584c9048e31552e1f14a1f3bf42
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-02 01:13:10 +0800

    refactor TagTopic#popular_links to hit database once only

--- rails/URLShortener/app/models/tag_topic.rb
+++ rails/URLShortener/app/models/tag_topic.rb
@@ -21,20 +21,11 @@ class TagTopic < ApplicationRecord
     source: :shortened_url
 
   def popular_links
-    # tagged_shortened_urls.sort_by do |url|
-    #   url.num_clicks
-    # end.last(5).reverse
     shortened_urls
       .joins(:visits)
       .group(:short_url, :long_url)
       .order('COUNT(visits.id) DESC')
       .select('short_url, COUNT(visits.id) AS num_visits')
       .limit(5)
-
-    # shortened_urls.joins(:visits)
-    #   .group(:short_url, :long_url)
-    #   .order('COUNT(visits.id) DESC')
-    #   .select('long_url, short_url, COUNT(visits.id) as number_of_visits')
-    #   .limit(5)
   end
 end
