9ec1d58c9ea53efbdb1f0ba1710606c68fb50475
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-04 16:11:46 +0800

    created model boilerplate

---
+++ rails/polls_app/app/models/answer_choice.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: answer_choices
+#
+#  id          :bigint           not null, primary key
+#  question_id :integer          not null
+#  text        :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class AnswerChoice < ApplicationRecord
+end
