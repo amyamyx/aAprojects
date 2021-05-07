3940cea79005a0e4a7791fdceb92d8a19a2c231f
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-07 15:57:16 +0800

    annotated routes

--- rails/art_share_api/config/routes.rb
+++ rails/art_share_api/config/routes.rb
@@ -1,20 +1,26 @@
 # == Route Map
 #
-#         Prefix Verb   URI Pattern                        Controller#Action
-#  user_artworks GET    /users/:user_id/artworks(.:format) artworks#index
-#          users GET    /users(.:format)                   users#index
-#                POST   /users(.:format)                   users#create
-#           user GET    /users/:id(.:format)               users#show
-#                PATCH  /users/:id(.:format)               users#update
-#                PUT    /users/:id(.:format)               users#update
-#                DELETE /users/:id(.:format)               users#destroy
-#       artworks POST   /artworks(.:format)                artworks#create
-#        artwork GET    /artworks/:id(.:format)            artworks#show
-#                PATCH  /artworks/:id(.:format)            artworks#update
-#                PUT    /artworks/:id(.:format)            artworks#update
-#                DELETE /artworks/:id(.:format)            artworks#destroy
-# artwork_shares POST   /artwork_shares(.:format)          artwork_shares#create
-#  artwork_share DELETE /artwork_shares/:id(.:format)      artwork_shares#destroy
+#           Prefix Verb   URI Pattern                              Controller#Action
+#    user_artworks GET    /users/:user_id/artworks(.:format)       artworks#index
+#    user_comments GET    /users/:user_id/comments(.:format)       comments#index
+#            users GET    /users(.:format)                         users#index
+#                  POST   /users(.:format)                         users#create
+#             user GET    /users/:id(.:format)                     users#show
+#                  PATCH  /users/:id(.:format)                     users#update
+#                  PUT    /users/:id(.:format)                     users#update
+#                  DELETE /users/:id(.:format)                     users#destroy
+# artwork_comments GET    /artworks/:artwork_id/comments(.:format) comments#index
+#         artworks POST   /artworks(.:format)                      artworks#create
+#          artwork GET    /artworks/:id(.:format)                  artworks#show
+#                  PATCH  /artworks/:id(.:format)                  artworks#update
+#                  PUT    /artworks/:id(.:format)                  artworks#update
+#                  DELETE /artworks/:id(.:format)                  artworks#destroy
+#   artwork_shares POST   /artwork_shares(.:format)                artwork_shares#create
+#    artwork_share DELETE /artwork_shares/:id(.:format)            artwork_shares#destroy
+#         comments POST   /comments(.:format)                      comments#create
+#          comment PATCH  /comments/:id(.:format)                  comments#update
+#                  PUT    /comments/:id(.:format)                  comments#update
+#                  DELETE /comments/:id(.:format)                  comments#destroy
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
