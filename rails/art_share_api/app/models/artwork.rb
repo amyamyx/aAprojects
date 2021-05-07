1dd9020baa556169ebeeb58e42cbf75ca6789d7a
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-07 12:21:26 +0800

    added comments associations

--- rails/art_share_api/app/models/artwork.rb
+++ rails/art_share_api/app/models/artwork.rb
@@ -28,5 +28,11 @@ class Artwork < ApplicationRecord
   has_many :shared_viewers,
     through: :artwork_shares,
     source: :viewer
+
+  has_many :comments,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: "Comment",
+    dependent: :destroy
   
 end
