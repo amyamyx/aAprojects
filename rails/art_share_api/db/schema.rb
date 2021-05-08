8967e9cb9bbf81d20ad7728da2f0c9e4ef388bf0
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-08 14:27:02 +0800

    added favorite column in artworks

--- rails/art_share_api/db/schema.rb
+++ rails/art_share_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_073636) do
+ActiveRecord::Schema.define(version: 2021_05_07_084043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_05_07_073636) do
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "favorite", default: false
     t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
