53828e2ea220cf85cefcdcef8d37eb04a3ad851b
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-13 14:01:51 +0800

    added collections and artwork_collecitons tables

--- rails/art_share_api/db/migrate/20210512030250_create_collections.rb
+++ rails/art_share_api/db/migrate/20210512030250_create_collections.rb
@@ -1,14 +1,12 @@
 class CreateCollections < ActiveRecord::Migration[5.2]
   def change
     create_table :collections do |t|
+      t.string :name, null: false
       t.integer :user_id, null: false
-      t.integer :artwork_id, null: false
 
       t.timestamps
     end
 
     add_index :collections, :user_id
-    add_index :collections, :artwork_id
-    add_index :collections, [:user_id, :artwork_id], unique: true
   end
 end
