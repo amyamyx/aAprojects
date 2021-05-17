4a159f7a69d01862ec96d96cb409ceee8d03fca5
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-17 23:58:13 +0800

    created cats table and model

---
+++ rails/Cats/app/models/cat.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require "action_view"
+
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  validates :birth_date, :color, :name, :sex, :description, presence: true
+  validates :color, inclusion: { in: %w(yellow black white grey), 
+    message: "can only be a set of accepted colors" }
+  validates :sex, inclusion: { in: [ "M", "F"], 
+    messeage: "can only be M or F" }
+
+  def age
+    distance_of_time_in_words_to_now(self.birth_date) + " old"
+  end
+end
+
+
