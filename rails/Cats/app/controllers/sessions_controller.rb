df01c9110b0596ac1b11b712db1749f83d92abe6
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-23 15:49:39 +0800

    added sessions and users controllers actions

---
+++ rails/Cats/app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user
+      session[:session_token] = @user.reset_session_token!
+      redirect_to cats_url
+    else
+      redirect_to new_session_url
+    end
+  end
+  
+  def destroy
+    render json: "in the destroy action!"
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- rails/Cats/app/models/user.rb
+++ rails/Cats/app/models/user.rb
@@ -1,7 +1,7 @@
 require "bcrypt"
 
 class User < ApplicationRecord
-  validates :username, :session_token, presence: true, uniquesness: true
+  validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: { messgae: "Password can't be blank" }
   validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
