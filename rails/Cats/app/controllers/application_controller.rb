48ed86cba1d58ec7e3ca407f1c5e89a4fbdc2994
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-23 22:53:26 +0800

    added login! and logout! to applicationcontroller

--- rails/Cats/app/controllers/application_controller.rb
+++ rails/Cats/app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
     return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+  
+  def logout!
+    @current_user = nil
+    session[:session_token] = nil
+  end
 end
--- rails/Cats/app/controllers/users_controller.rb
+++ rails/Cats/app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+      login!(@user)
       redirect_to cats_url
     else
       render json: @user.errors.full_messages
