3edc421dd8a6c933ff756977d58b67da4a190854
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-27 00:39:35 +0800

    Created users table and model

---
+++ rails/music_app/app/models/user.rb
@@ -0,0 +1,38 @@
+require "bcrypt"
+
+class User < ApplicationRecord
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+  attr_reader :password
+
+  def self.generate_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_token!
+    self.session_token = User.generate_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end
