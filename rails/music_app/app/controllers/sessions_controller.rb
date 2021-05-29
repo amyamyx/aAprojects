99346b9b8e831e1a88aad1a0dd0f02a71f2ccb93
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-29 12:40:07 +0800

    redirect to bands index after loggin in

--- rails/music_app/app/controllers/sessions_controller.rb
+++ rails/music_app/app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 
     if user
       login!(user)
-      redirect_to user_url(user)
+      redirect_to bands_url
     else
       flash.now[:errors] = ["Wrong credentials!"]
       render :new
