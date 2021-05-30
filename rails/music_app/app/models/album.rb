c5eb553901b1f607d90a8460badb3cbeafe6a4ba
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-31 00:02:24 +0800

    remove presence validation for :live

--- rails/music_app/app/models/album.rb
+++ rails/music_app/app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ApplicationRecord
-  validates :title, :year, :live, presence: true
+  validates :title, :year, presence: true
   validates :band_id, uniqueness: { scope: :title }
-  validates :live, inclusion: [true, false]
+  validates :live, inclusion: {in: [true, false] }
 
   belongs_to :band
 end
