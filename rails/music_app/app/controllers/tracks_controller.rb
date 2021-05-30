c6753b691d858274142e20591284d1b768d5549e
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-31 00:39:57 +0800

    add tracks controller actions

---
+++ rails/music_app/app/controllers/tracks_controller.rb
@@ -0,0 +1,55 @@
+class TracksController < ApplicationController
+  before_action :ensure_logged_in
+
+  def new
+    @track = Track.new
+    @album = Album.find(params[:album_id])
+    @albums = @album.band.albums
+  end
+
+  def create
+    @track = Track.new(track_params)
+
+    if @track.save
+      redirect_to track_url(@track)
+    else
+      flash[:errors] = @track.errors.full_messages
+      redirect_to new_album_track_url(@track.album)
+    end
+  end
+
+  def edit
+    @track = Track.find(params[:id])
+    @album = Album.find(@track.album_id)
+    @albums = @album.band.albums
+  end
+
+  def update
+    @track = Track.find(params[:id])
+
+    if @track.update(track_params)
+      redirect_to track_url(@track)
+    else
+      flash[:errors] = @track.errors.full_messages
+      redirect_to edit_track_url(@track)
+    end
+  end
+
+  def show
+    @track = Track.find(params[:id])
+    # @notes = @track.notes
+    @album = @track.album
+  end
+
+  def destroy
+    track = Track.find(params[:id])
+    track.destroy
+    redirect_to album_url(track.album_id)
+  end
+
+  private
+  
+  def track_params
+    params.require(:track).permit(:title, :ord, :album_id, :lyrics, :bonus)
+  end
+end
