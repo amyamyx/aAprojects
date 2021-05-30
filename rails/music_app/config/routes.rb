9e53a7372f91ee90aa8472a62cf49b4dbd83436c
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-31 01:47:57 +0800

    add notes routes

--- rails/music_app/config/routes.rb
+++ rails/music_app/config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
-  resources :tracks, except: [:index, :new]
   
   resources :bands do
     resources :albums, only: [:new]
   end
-
+  
   resources :albums, except: [:index, :new] do
     resources :tracks, only: [:new]
   end
+
+  resources :tracks, except: [:index, :new] do
+    resources :notes, only: [:create]
+  end
+  
+  resources :notes, only: [:destroy]
 end
