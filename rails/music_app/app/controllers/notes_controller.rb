1dacd1dfd3d6d802294fefe0244446fd5881d11f
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-31 01:48:19 +0800

    add notes controller actions

---
+++ rails/music_app/app/controllers/notes_controller.rb
@@ -0,0 +1,31 @@
+class NotesController < ApplicationController
+  before_action :ensure_logged_in
+  before_action :ensure_owner, only: [:destroy]
+
+  def create
+    note = Note.new(note_params)
+    note.track_id = params[:track_id]
+    note.user_id = current_user.id
+
+    note.save
+    flash[:errors] = note.try(:errors).try(:full_messages)
+    redirect_to track_url(note.track_id)
+  end
+
+  def destroy
+    note = Note.find(params[:id])
+    note.destroy
+    redirect_to track_url(note.track_id)
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:body)
+  end
+
+  def ensure_owner
+    note = note.find(params[:id])
+    redirect_to track_url(note.track_id) unless current_user.id == note.user_id
+  end
+end
