3b251ad96853b384876f02754ea254b546bbb570
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-05-31 21:40:05 +0800

    add lyrics helper

--- rails/music_app/app/helpers/tracks_helper.rb
+++ rails/music_app/app/helpers/tracks_helper.rb
@@ -2,4 +2,13 @@ module TracksHelper
   def track_select_helper(album_1, album_2)
     "selected".html_safe if album_1.id == album_2.id
   end
+
+  def ugly_lyrics(lyrics)
+    formatted_lyrics =""
+    lyrics.lines.each do |line|
+      formatted_lyrics += "&#9835; #{ h(line) }"
+    end
+
+    "<pre class=\"lyrics\">#{ formatted_lyrics }</pre>".html_safe
+  end
 end
