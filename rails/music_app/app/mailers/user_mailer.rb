c58c0e2e1003cf61e99544433e9dd00c1c441c7f
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-06-04 23:53:38 +0800

    add welcom_email mailer and views

--- rails/music_app/app/mailers/user_mailer.rb
+++ rails/music_app/app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ApplicationMailer
   def activate_email(user)
     @user = user
-    @url = "http://localhost:3000/users/#{ user.id }/activate"
+    @url = "http://localhost:3000/users/activate?activation_token=#{ @user.activation_token }"
 
     mail(to: user.email, subject: "Activate your account now!")
   end
 
+  def welcome_email(user)
+    @user = user
+    @url = "http://localhost:3000/session/new"
+
+    mail(to: user.email, subject: "Welcome to Music APP!!")
+  end
 end
