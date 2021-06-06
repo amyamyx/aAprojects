9c1c75dddb24738671dfd1b9031dbb47ba702e87
Tiffany Tang <amyamyxtiffany@gmail.com>
2021-06-06 21:39:28 +0800

    add search class method to Tag

--- rails/music_app/app/models/tag.rb
+++ rails/music_app/app/models/tag.rb
@@ -1,6 +1,12 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
-  validates :name, uniqueness: { scope: [:taggable_id, :taggalbe_type] }
+  validates :name, uniqueness: { scope: [:taggable_id, :taggable_type] }
 
   belongs_to :taggable, polymorphic: true
+
+  def self.search(name, model = nil)
+    tags = Tag.where("name LIKE ?", "%#{ name }%").includes(:taggable)
+    tags = tags.select { |tag| tag.taggable_type == model } if model
+    tags.map { |tag| tag.taggable }
+  end
 end
